6f5d9c58f63b1ceb03f398094df5068ac80b1a3a
El Toro Maestro <eltoro@atomlogik.de>
2024-02-09 16:02:13 -0500

    initial

---
+++ lib/swiss_army_knife.ex
@@ -0,0 +1,18 @@
+defmodule SwissArmyKnife do
+  @moduledoc """
+  Documentation for `SwissArmyKnife`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SwissArmyKnife.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/swiss_army_knife_test.exs
@@ -0,0 +1,8 @@
+defmodule SwissArmyKnifeTest do
+  use ExUnit.Case
+  doctest SwissArmyKnife
+
+  test "greets the world" do
+    assert SwissArmyKnife.hello() == :world
+  end
+end
